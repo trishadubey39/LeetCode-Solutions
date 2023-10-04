3bad4de1c6fd6432e243f688735df1da629c6970
Trisha Dubey <112706788+trishadubey39@users.noreply.github.com>
2023-10-04 18:43:46 +0530

    Time: 15 ms (83.30%), Space: 47.1 MB (75.73%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,66 +1,48 @@
 class MyHashMap {
 
-    private List<List<Pair<Integer, Integer>>> mp;
-    private int size = 10;
+    private static final int SIZE = 1000;
+    private List<int[]>[] map;
 
     public MyHashMap() {
-        mp = new ArrayList<>(size);
-        for (int i = 0; i < size; i++) {
-            mp.add(new LinkedList<>());
+        map = new ArrayList[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            map[i] = new ArrayList<>();
         }
-    }
-
-    private int hash(int key) {
-        return key % size;
     }
 
     public void put(int key, int value) {
-        int i = hash(key);
-        List<Pair<Integer, Integer>> bucket = mp.get(i);
-
-        for (Pair<Integer, Integer> pair : bucket) {
-            if (pair.first.equals(key)) {
-                pair.second = value;
+        int index = key % SIZE;
+        List<int[]> bucket = map[index];
+        for (int[] pair : bucket) {
+            if (pair[0] == key) {
+                pair[1] = value; 
                 return;
             }
         }
-
-        bucket.add(new Pair<>(key, value));
+        bucket.add(new int[]{key, value}); 
     }
 
     public int get(int key) {
-        int i = hash(key);
-        List<Pair<Integer, Integer>> bucket = mp.get(i);
-
-        for (Pair<Integer, Integer> pair : bucket) {
-            if (pair.first.equals(key)) {
-                return pair.second;
+        int index = key % SIZE;
+        List<int[]> bucket = map[index];
+        for (int[] pair : bucket) {
+            if (pair[0] == key) {
+                return pair[1]; 
             }
         }
-
-        return -1;
+        return -1; 
     }
 
     public void remove(int key) {
-        int i = hash(key);
-        List<Pair<Integer, Integer>> bucket = mp.get(i);
-
-        for (Pair<Integer, Integer> pair : bucket) {
-            if (pair.first.equals(key)) {
-                bucket.remove(pair);
+        int index = key % SIZE;
+        List<int[]> bucket = map[index];
+        for (int i = 0; i < bucket.size(); i++) {
+            int[] pair = bucket.get(i);
+            if (pair[0] == key) {
+                bucket.remove(i); 
                 return;
             }
         }
-    }
-}
-
-class Pair<K, V> {
-    K first;
-    V second;
-
-    public Pair(K first, V second) {
-        this.first = first;
-        this.second = second;
     }
 }
 
