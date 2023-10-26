2303e63bc803c2136c4fec24c458c6e6673ee001
Trisha Dubey <112706788+trishadubey39@users.noreply.github.com>
2023-10-26 23:29:26 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (83.91%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,44 +1,29 @@
 class Solution {
-    private List<String> solve(String digits, String[] arr) {
-        if (digits.length() == 0) {
-            List<String> bs = new ArrayList<>();
-            bs.add("");
-            return bs;
-        }
-
-        char c = digits.charAt(0);
-        String a = arr[c - '0'];
-        String smallinput = digits.substring(1);
-        List<String> rest = solve(smallinput, arr);
-        List<String> res = new ArrayList<>();
-        for (String x : rest) {
-            for (char x1 : a.toCharArray()) {
-                res.add(x1 + x);
-            }
-        }
-        return res;
-    }
-
-    private List<String> ans = new ArrayList<>();
-
-    private void solve2(String digits, String[] arr, int i, String com) {
-        if (i == digits.length()) {
-            ans.add(com);
-            return;
-        }
-        char c = digits.charAt(i);
-        String a = arr[c - '0'];
-        for (char x1 : a.toCharArray()) {
-            solve2(digits, arr, i + 1, com + x1);
-        }
-    }
+    ArrayList<String> arl = new ArrayList();
+    String[] arr =  {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 
     public List<String> letterCombinations(String digits) {
-        List<String> a = new ArrayList<>();
-        if (digits.length() == 0)
-            return a;
-        String[] arr = {"0", "0", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        solve2(digits, arr, 0, "");
-        return ans;
+        if(digits.equals("")){
+            return arl;
+        }
+        helper(0,new StringBuilder(""),digits);
+        return arl;
+    }
+
+
+    void helper(int index, StringBuilder s,String digits){
+        // System.out.println(s);
+        if(s.length() == digits.length()){
+            //System.out.println(s);
+            arl.add(new String(s));
+            return;
+        }
+        int a = digits.charAt(index) - '0';
+        for(int i = 0 ; i < arr[a].length() ; i++){
+        s.append((arr[a].charAt(i)));
+        helper(index+1,s,digits);
+        s.deleteCharAt(s.length()-1);
+
+        }
     }
 }
