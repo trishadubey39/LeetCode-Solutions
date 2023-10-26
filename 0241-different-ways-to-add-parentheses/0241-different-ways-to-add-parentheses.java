0c340c6de41ffc3f9bdc7522c60d6d6bd31edfaa
Trisha Dubey <112706788+trishadubey39@users.noreply.github.com>
2023-10-26 10:50:10 +0530

    Time: 3 ms (38.67%), Space: 41.9 MB (25.12%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
@@ -1,27 +1,31 @@
 class Solution {
     public List<Integer> diffWaysToCompute(String expression) {
-        List<Integer> res = new LinkedList<>();
-        for(int i=0; i<expression.length(); i++) {
-            char c = expression.charAt(i);
-            if(c == '-' || c == '+' || c == '*') {
-                List<Integer> left = diffWaysToCompute(expression.substring(0, i));
-                List<Integer> right = diffWaysToCompute(expression.substring(i+1));
-                for(int a : left) {
-                    for(int b : right) {
-                        if(c == '-') {
-                            res.add(a - b);
-                        } else if(c =='+') {
-                            res.add(a + b);
-                        } else {
-                            res.add(a * b);
-                        }
-                    }
-                }
+       List<Integer> result = new ArrayList<>();
+        for(int i=0; i < expression.length(); i++){
+            char curr = expression.charAt(i);
+            if (!Character.isDigit(curr)){
+                List<Integer> leftParts = diffWaysToCompute(expression.substring(0, i));
+                List<Integer> rightParts = diffWaysToCompute(expression.substring(i+1));
+                evaluate(leftParts, rightParts, curr, result);
             }
         }
-        if(res.isEmpty()) {
-            res.add(Integer.parseInt(expression));
-        }
-        return res;
+        if(isNumeric(expression)) result.add(Integer.valueOf(expression));
+        return result;
+    }
+    
+    private boolean isNumeric(String s){
+        for(char c : s.toCharArray()) 
+            if(!Character.isDigit(c)) return false;
+        return true;
+    }
+    
+    private void evaluate(List<Integer> leftParts, List<Integer> rightParts, char operand,             List<Integer> result){
+        for(Integer left : leftParts){
+            for(Integer right : rightParts){
+                if(operand == '+') result.add(left + right);
+                else if(operand == '-') result.add(left - right);
+                else result.add(left * right);
+            }
+        }        
     }
 }
