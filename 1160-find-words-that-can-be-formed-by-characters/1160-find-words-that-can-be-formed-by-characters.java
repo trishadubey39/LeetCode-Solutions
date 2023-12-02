d47254360b09c5df78b97943d965ebaf6bdc0380
Trisha Dubey <112706788+trishadubey39@users.noreply.github.com>
2023-12-02 17:39:04 +0530

    Time: 2 ms (100.00%), Space: 44.1 MB (56.30%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
@@ -1,30 +1,31 @@
 class Solution {
-
-    private int [] count(String s){
-        int[] arr = new int [26];
-        for(char ch : s.toCharArray()){
-            arr[ch-'a']++;
-        }
-        return arr;
-    }
-
-    private boolean check(int[] chars, int[] word){
-        for(int i=0; i<26; i++){
-            if(chars[i] < word[i]) return false;
-        }
-        return true;
-    }
-
     public int countCharacters(String[] words, String chars) {
-        int []cnt_char = count(chars);
-        int res=0;
-        for(String word : words){
-            int []cnt_word = count(word);
-
-            if(check(cnt_char, cnt_word)){
-                res += word.length();
-            }
+        int[] counts = new int[26];
+        for(int i=0;i<chars.length();i++)
+        {
+            counts[chars.charAt(i)-'a']++;
+        }
+        int res =0 ;
+        
+        for(String s : words)
+        {
+            if(canForm(s,counts))
+                res+=s.length();
         }
         return res;
+    }
+
+    boolean canForm(String word, int[] counts)
+    {
+        int[] c = new int[26];
+        
+        for(int i =0;i<word.length();i++)
+        {
+            int x = word.charAt(i)-'a';
+            c[x]++;
+            if(c[x] > counts[x])
+                return false;
+        }
+        return true;
     }
 }
