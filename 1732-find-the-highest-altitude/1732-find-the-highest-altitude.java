1110b705a3dd74cb4788f6421aacdc72eb620e56
Trisha Dubey <112706788+trishadubey39@users.noreply.github.com>
2023-12-07 23:31:15 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (52.01%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int max = 0;
+        int current = 0;
+
+        for (int i = 0; i < gain.length; i++) {
+            current += gain[i]; 
+            max = Math.max(max, current);
+        }
+        
+        return max;
+    }
+}
