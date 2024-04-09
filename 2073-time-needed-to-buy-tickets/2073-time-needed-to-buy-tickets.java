d8315b5dc29c2928704aaf144da8ace229231e89
Trisha Dubey <112706788+trishadubey39@users.noreply.github.com>
2024-04-09 19:46:48 +0530

    Time: 4 ms (34.15%), Space: 40.9 MB (86.99%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int count = 0; // Variable to store the time
+        int index = 0; // Variable to iterate through the array
+        
+        // Run the loop until the value at the k index becomes zero
+        while (tickets[k] != 0) {
+            if (tickets[index] > 0) {
+                count++;
+                tickets[index]--;
+                index = (index + 1) % tickets.length;
+            } else if (tickets[index] == 0) {
+                index = (index + 1) % tickets.length;
+            }
+        }
+        
+        return count;
+    }
+}
